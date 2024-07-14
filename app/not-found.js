9000e74263230b5845a9c6371a1790b4254d0d9c
Shoaib Ansari <app@internationalschooling.org>
2024-07-14 15:20:04 +0530

    404 finally done

--- app/not-found.js
+++ app/not-found.js
@@ -8,28 +8,49 @@ import React from 'react'
 const NotFound = () => {
     return (
         <Page>
-            <div className='relative h-[100svh] flex flex-col items-center justify-center ' >
+            <div className='h-[100svh] flex flex-col relative items-center justify-center ' >
                 <GridBackground />
-                <CardContainer>
-                    <CardBody>
-                        <div className='p-4 relative border backdrop-blur shadow-2xl border-gray-200 dark:border-gray-800 w-4/5 mx-auto md:max-w-4/5 md:w-96'>
-                            <Plus size={24} className='-top-3 -right-3 absolute' />
-                            <Plus size={24} className='-top-3 -left-3 absolute' />
-                            <Plus size={24} className='-bottom-3 -right-3 absolute' />
-                            <Plus size={24} className='-bottom-3 -left-3 absolute' />
+                <CardContainer className="inter-var">
+                    <CardBody className="bg-white dark:bg-black relative group/card shadow-xl  hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1]  dark:border-white/[0.2] border-black/[0.1] w-4/5 md:w-96 h-auto p-6 border  ">
+                        <Plus size={24} className='-top-3 -right-3 absolute' />
+                        <Plus size={24} className='-top-3 -left-3 absolute' />
+                        <Plus size={24} className='-bottom-3 -right-3 absolute' />
+                        <Plus size={24} className='-bottom-3 -left-3 absolute' />
+                        <CardItem translateZ="100" className="w-full mt-4 pointer-events-none">
+                            <h3 className='text-5xl md:text-8xl'>404</h3>
+                        </CardItem>
+                        <CardItem
+                            translateZ="50"
+                            className="pointer-events-none text-xl font-bold text-neutral-600 dark:text-white"
+                        >
+                            Lost in the Digital Bermuda Triangle
+                        </CardItem>
+                        <CardItem
+                            as="p"
+                            translateZ="60"
+                            className="pointer-events-none mb-6 text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                        >
+                            Oh no! It seems the page you're looking for has taken a vacation to the Digital Bermuda Triangle. Our team of tech wizards is summoning it back with some digital voodoo.
+                        </CardItem>
+                        <CardItem
+                            translateZ={20}
+                            as={Link}
+                            href={"/"}
+                            target="__blank"
+                            className="btn btn-sm font-normal dark:text-white"
+                        >
+                            <Home size={18} /> Go to Home →
+                        </CardItem>
 
-                            <CardItem rotateX={40} rotateY={-10}>
-                                <h2 className='text-4xl md:text-8xl'>404</h2>
-                            </CardItem>
-                            <p className='text-xl mb-2'>Lost in the Digital Bermuda Triangle</p>
-                            <p className='text-xs mb-4'>Oh no! It seems the page you're looking for has taken a vacation to the Digital Bermuda Triangle. Our team of tech wizards is summoning it back with some digital voodoo.</p>
-                            <Link href={"/"} className='btn btn-sm'><Home size={18} /> Got to homepage</Link>
-                        </div>
                     </CardBody>
                 </CardContainer>
             </div>
-        </Page>
+        </Page >
     )
 }
 
 export default NotFound
+
+
+
+
